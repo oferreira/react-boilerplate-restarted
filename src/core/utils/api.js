3e49f72916b851cab8a4df6341071c2d31ec019c
Olivier ferreira <olivier@mediainvest.io>
2018-01-07 18:47:41 +0100

    add utils for create an url

---
+++ src/core/utils/api.js
@@ -0,0 +1,18 @@
+export function createUrl(url, params = {}) {
+  const keys = Object.keys(params)
+  if (keys.length) {
+    keys.forEach((k) => {
+      if (typeof params[k] === 'object') {
+        let flatten = ''
+        params[k].forEach((ref) => {
+          flatten += (flatten === '' ? ref : `&${k}=${ref}`)
+        })
+        url = url.replace(`{${k}}`, flatten) // eslint-disable-line
+      } else {
+        url = url.replace(`{${k}}`, params[k]) // eslint-disable-line
+      }
+    })
+  }
+  return url
+}
+
