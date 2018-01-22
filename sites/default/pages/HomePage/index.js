c227de39d874dd2982e16d7065fc500d6d82d2f8
Oudinyt <oudinyt@gmail.com>
2018-01-22 15:30:11 +0100

    brands init - create heading style, body, link (core)

--- sites/default/pages/HomePage/index.js
+++ sites/default/pages/HomePage/index.js
@@ -11,7 +11,7 @@ import Header from 'common/components/Header'
 import SelectLanguages from 'core/language/containers/SelectLanguages'
 import Translation from 'core/language/components/Translation'
 
-import H1 from 'common/components/H1'
+import Icon from 'common/components/Icon'
 import Button from 'common/components/Button'
 import './style.scss'
 
@@ -23,29 +23,85 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
         <Header>
           <BurgerToggle />
         </Header>
-        <BurgerMenu />
-        <div className="container">
+
+        <div>
+
+          <Translation id="app.CityTax.taxesAndChargeIncluded" />
           <Node id={22} />
           <SelectLanguages />
-          <Translation id="app.CityTax.taxesAndChargeIncluded" />
 
 
-          <div className="blockCms blockOurBrands">
-            <H1 playful>Our brands</H1>
-            <h1 className="blockOurBrands__title">Our brands</h1>
+          <h1 className="heading-primary-1">heading-primary-1</h1>
+          <h1 className="heading-primary-2">heading-primary-2</h1>
+          <h1 className="heading-primary-3">heading-primary-3</h1>
 
-            <div className="blockOurBrands__brand">
-              <img className="blockOurBrands__img" src="../../assets/brands/royal-tulip.jpg" alt="Logo Royal Tulip - luxury hotel" />
-              <p className="blockOurBrands__txt">Midscale properties</p>
 
-              <Button rounded small>
-                Learn more
-              </Button>
+          <h1 className="heading-secondary-1">heading-secondary-1</h1>
+          <h1 className="heading-secondary-2">heading-secondary-2</h1>
+          <h1 className="heading-secondary-3">heading-secondary-3</h1>
+
+
+          <p className="body-1">body-1</p>
+          <p className="body-2">body-2</p>
+          <p className="body-3">body-3</p>
+
+          <a href="http://www.google.com" className="link">Link </a>
+
+        </div>
+
+
+        <BurgerMenu />
+        <div className="WrapperPage">
+
+          <div className="blockCms blockBrands">
+
+            <div className="blockBrands__wrapper">
+
+              <h1 className="blockBrands__title">Our brands</h1>
+
+              <a href="http://www.google.com" title="Brand - Royal Tulip " className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/royal-tulip.jpg" alt="Logo - Royal Tulip - luxury hotel" />
+                <p className="blockBrands__txt">Midscale properties</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+              <a href="http://www.google.com" title="Brand - Golden tulip" className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/golden-tulip.jpg" alt="Logo - Golden tulip" />
+                <p className="blockBrands__txt">Midscale properties</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+              <a href="http://www.google.com" title="Brand -Tulip Inn" className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/tulip-inn.jpg" alt="Logo - Tulip Inn" />
+                <p className="blockBrands__txt">Midscale properties</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+              <a href="http://www.google.com" title="View all Louvre hotel Group brands" className="blockBrands__Link">
+                <span>View all Louvre hotel Group brands</span>
+                <Icon name="arrow-bot" />
+              </a>
+
+              <h2 className="blockBrands__subTitle">Our hotel partner</h2>
+
+              <a href="http://www.google.com" title="Brand - Magnuson " className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/magnuson.jpg" alt="Logo - Magnuson worldwide" />
+                <p className="blockBrands__txt">Americ’as fastest growing hotel brand</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
 
             </div>
 
-
           </div>
+
 
         </div>
         <Footer menuId="footer" />
