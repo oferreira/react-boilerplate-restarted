075c1c9f07e1fbec2022498fa7a39075530b61f4
jessica curpen <jessica.h.curpen@gmail.com>
2018-01-24 15:19:16 +0100

    ADD miles partners

--- sites/default/layouts/Miles/index.js
+++ sites/default/layouts/Miles/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MilesHeader from './MilesHeader'
-import MilesAdvantages from './MilesAdvantages'
+import MilesPartners from './MilesPartners'
 
 import './styles.scss'
 
@@ -8,7 +8,7 @@ const Miles = () => (
   <div>
     <MilesHeader />
     <div className="PartnerLink"><a href="/"> Missing Frequent flyers miles </a></div>
-    <MilesAdvantages />
+    <MilesPartners />
   </div>
 )
 
