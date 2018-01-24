ac0d2cd7065fd0fbb26fd9af53b4bdc43eb5d7fd
jessica curpen <jessica.h.curpen@gmail.com>
2018-01-24 16:58:23 +0100

    ADD node & dev api

--- sites/default/layouts/Miles/MilesHeader/index.js
+++ sites/default/layouts/Miles/MilesHeader/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Img from 'common/PlaceHolders/Img'
 import Text from 'common/PlaceHolders/Text'
 
+import Node from 'drupal/containers/Node'
+
 import './styles.scss'
 
 const MilesHeader = () => (
   <div>
+    <Node id={68} fields={['body']} />
     <div className="PartnerHeader">
       <div className="PartnerHeader__Name">
         <h1>Miles reward program</h1>
