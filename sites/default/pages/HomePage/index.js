6dab29fd5af1b71e02c8e00b2b4e69d10d61c60d
Oudinyt <oudinyt@gmail.com>
2018-01-24 16:48:52 +0100

    brands init - create heading style, body, link (core)

--- sites/default/pages/HomePage/index.js
+++ sites/default/pages/HomePage/index.js
@@ -121,6 +121,95 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
 
           </div>
 
+          {/*Bloc history*/}
+          <div className="blockCms blockTimeline">
+            <div className="blockTimeline__img">
+              <img src="https://dummyimage.com/571x357/adadad/525252" alt="" />
+            </div>
+            <div className="blockTimeline__txt">
+              <h2 className="blockTimeline__title">Golden Tulip<br/>through the ages</h2>
+              <p className="blockTimeline__intro">Golden Tulip, a prestigious heritage with an upscale positioning from the begining</p>
+              <ul className="blockTimeline__lists">
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+                <li className="blockTimeline__list">
+                  <h4 className="blockTimeline__listTitle">2015</h4>
+                  <p className="blockTimeline__listDesc">Golden Tulipis owned by Jin Jiang international</p>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+
+
+          <div className="blockCms blockBrands">
+
+            <div className="blockBrands__wrapper">
+
+              <h1 className="blockBrands__title">
+                Other brands
+              </h1>
+              <p className="blockBrands__intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> In massa augue, finibus semper sagittis malesuada</p>
+
+              <a href="http://www.google.com" title="Brand - Royal Tulip " className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/royal-tulip.jpg" alt="Logo - Royal Tulip - luxury hotel" />
+                <p className="blockBrands__txt">Midscale properties</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+              <a href="http://www.google.com" title="Brand -Tulip Inn" className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/tulip-inn.jpg" alt="Logo - Tulip Inn" />
+                <p className="blockBrands__txt">Midscale properties</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+              <a href="http://www.google.com" title="View all Louvre hotel Group brands" className="blockBrands__Link">
+                <span>View all Louvre hotel Group brands</span>
+                <Icon name="arrow-bot" />
+              </a>
+
+              <h2 className="blockBrands__subTitle">Our hotel partner</h2>
+              <p className="blockBrands__intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> In massa augue, finibus semper sagittis malesuada</p>
+
+              <a href="http://www.google.com" title="Brand - Magnuson " className="blockBrands__brand">
+                <img className="blockBrands__img" src="../../assets/brands/magnuson.jpg" alt="Logo - Magnuson worldwide" />
+                <p className="blockBrands__txt">Americ’as fastest growing hotel brand</p>
+                <Button rounded small>
+                  Learn more
+                </Button>
+              </a>
+
+            </div>
+
+          </div>
+
+
         </div>
         <Footer menuId="footer" />
       </div>)
